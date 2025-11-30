079f02d43d5cd82bec638191eb266aad0b305391
SongThun <nguyenngocsongthuong2004@gmail.com>
2025-11-30 12:22:45 +0700

    check deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True").lower() in ("true", "1", "yes")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = [
+    h.strip() for h in os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+]
 
 
 # Application definition
